67e0a4e18787e144d36764b51e949c4df0a7cdf0
VVUBYA <105579805+VVUBYA@users.noreply.github.com>
2024-10-04 13:01:46 +0300

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,51 +1,53 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
 // Interface for the Uniswap V2 Router
 interface IUniswapV2Router {
     function swapExactETHForTokens(
-        uint amountOutMin,
+        uint256 amountOutMin,
         address[] calldata path,
         address to,
-        uint deadline
-    ) external payable returns (uint[] memory amounts);
-}
+        uint256 deadline
+    ) external payable returns (uint256[] memory amounts);
 
-interface IERC20 {
-    function balanceOf(address account) external view returns (uint);
-    function transfer(address recipient, uint amount) external returns (bool);
+    function WETH() external pure returns (address); // Add WETH() declaration
 }
 
 contract TokenBuyer {
     address public owner;
     IUniswapV2Router public uniswapRouter;
     address public token;
-       uint public unlockTime;
-    address public owner;
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Not the contract owner");
         _;
     }
 
-    constructor(address _uniswapRouter, address _token, uint _unlockTime) {
-        owner = msg.sender; // Use = for assignment, not ==
+    constructor(address _uniswapRouter, address _token) {
+        owner = msg.sender;
         uniswapRouter = IUniswapV2Router(_uniswapRouter);
         token = _token;
-        require(_unlockTime > block.timestamp, "Unlock time should be in the future");
-        unlockTime = _unlockTime;
-        owner = msg.sender;
     }
 
-     
-
- 
     // Function to buy tokens from the DEX
-    function buyToken(uint amountOutMin, uint deadline) external payable onlyOwner {
+    function buyToken(uint256 amountOutMin, uint256 deadline) external payable onlyOwner {
         require(msg.value > 0, "Insufficient ETH provided");
 
-        address[] memory path = new address[](2); // Declare and initialize the path array
-        path[0] = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2; // WETH address on mainnet (change for other networks)
+        // Initialize path array
+        address[] memory path = new address[](2);
+        path[0] = uniswapRouter.WETH(); // WETH address (Wrapped ETH)
         path[1] = token;
 
         // Perform the swap
@@ -59,15 +61,15 @@ contract TokenBuyer {
 
     // Function to withdraw all tokens to the owner's wallet
     function withdrawTokens() external onlyOwner {
-        uint tokenBalance = IERC20(token).balanceOf(address(this));
+        uint256 tokenBalance = IERC20(token).balanceOf(address(this));
         require(tokenBalance > 0, "No tokens to withdraw");
 
-        IERC20(token).transfer(owner, tokenBalance);
+        require(IERC20(token).transfer(owner, tokenBalance), "Token transfer failed");
     }
 
     // Function to withdraw any leftover ETH
     function withdrawETH() external onlyOwner {
-        uint ethBalance = address(this).balance;
+        uint256 ethBalance = address(this).balance;
         require(ethBalance > 0, "No ETH to withdraw");
 
         (bool success, ) = owner.call{value: ethBalance}("");
