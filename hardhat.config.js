19b67a138a63686c94eb0cbfeaf82c15f2bba6a2
AmiasYaska <118506048+AmiasYaska@users.noreply.github.com>
2024-10-05 19:45:34 +0300

    added test scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,40 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const BSC_TESTNET_API_KEY = process.env.BSC_TESTNET_API_KEY;
+
 module.exports = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    anvil: {
+      url: "http://localhost:8545",
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    bscTestnet: {
+      url: `https://data-seed-prebsc-1-s1.binance.org:8545`, // Binance Smart Chain Testnet URL
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 20000,
+  },
 };
